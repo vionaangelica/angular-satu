6861ad43bb1dce38af663241368e9eed06ff0619
vionaangelica <c14180157@john.petra.ac.id>
2021-02-23 17:59:48 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  a =0;
+  b=0;
+  item=[
+    {nama :"Jeruk", harga :"15000", satuan:"kg"},
+    {nama :"Apel", harga :"20000", satuan:"kg"}
+  ]
+
+  tampil : boolean = false;
 }
